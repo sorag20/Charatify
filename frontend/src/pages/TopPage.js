5b772c828bb15ae5f3eb77f98482be0a8af352b5
hirafish <95068237+hirafish@users.noreply.github.com>
2023-06-04 15:12:40 +0900

    topページに動きを付けました

--- frontend/src/pages/TopPage.js
+++ frontend/src/pages/TopPage.js
@@ -9,18 +9,22 @@ const TopPage = () => {
   }, [navigate]);
 
   return (
-    <div className={styles.topPage}>
-      <img className={styles.rectangleIcon} alt="" src="/rectangle@2x.png" />
-      <div className={styles.letsPlay}>Let’s play!</div>
-      <div className={styles.charatify}>Charatify</div>
-      <div className={styles.cibspotify} />
-      <img className={styles.vectorIcon} alt="" src="/vector.svg" />
-      <img
-        className={styles.httpsapplottiefilescomanIcon}
-        alt=""
-        src="/httpsapplottiefilescomanimation975fb9d697ea4e7da1b7ed243168cde4@2x.png"
-        onClick={onHttpsapplottiefilescomanImageClick}
-      />
+    <div class="top">
+      <div className={styles.topPage}>
+        <img className={styles.rectangleIcon} alt="" src="/rectangle@2x.png" />
+        <div className={styles.letsPlay}>Let’s play!</div>
+        <div className={styles.charatify}>Charatify</div>
+        <div className={styles.cibspotify} />
+        <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+        <div class="button">
+          <img
+            className={`${styles.httpsapplottiefilescomanIcon} ${styles.button}`}
+            alt=""
+            src="/httpsapplottiefilescomanimation975fb9d697ea4e7da1b7ed243168cde4@2x.png"
+            onClick={onHttpsapplottiefilescomanImageClick}
+          />
+        </div>
+      </div>
     </div>
   );
 };
